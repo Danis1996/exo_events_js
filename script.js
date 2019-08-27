81023d32f1cfa4f779b09a4dad7a33a6de60a9e0
danis1996 <danisandrios@gmail.com>
2019-08-27 11:02:13 +0300

    exo 1 à 9

--- script.js
+++ script.js
@@ -36,13 +36,9 @@ hamburgerMenuButton.addEventListener("click", onButtonClick);
     editButton2.addEventListener("click", onEditButtonClick2);
 
 // 5) si un utilisateur double clique sur la navbar en haut, tout Bootstrap disparaît
-	var navbar = document.querySelector('header');
-	console.log(navbar)
-	
+	var navbar = document.querySelector('header');	
 	allBotstrap = document.querySelector('link')
 	var doubleClickButton = function() {
-		console.log(allBotstrap)
-		console.log("TONGA AAAA")
 		allBotstrap.href = '';
 		imageLink = document.getElementsByClassName('card-img-top')
 		for(let i=0; i<= imageLink.length - 1; i++){
@@ -52,13 +48,49 @@ hamburgerMenuButton.addEventListener("click", onButtonClick);
 	navbar.addEventListener("dblclick", doubleClickButton)
 
 //6)si un utilisateur passe sa souris sur le bouton "View" d'une card (n'importe laquelle), celle-ci va se réduire.
-	var btn = document.getElementsByClassName('btn');
+	var detector = 0
+	let btn = document.getElementsByClassName('btn');
 	for(let i=2; i<= btn.length - 1; i=i+2){
-		console.log(btn[i])
-		btn[i].onclick = function(){
-			for(let n=0; n<= btn.length - 1; n++){
-			 var cardTexta = document.getElementsByClassName("card-text")[n];
-			 	cardTexta.textContent = "";
+		btn[i].onmouseover = function(){
+			detector ++
+		for(let n=0; n < 6; n++){
+			let cardTexta = document.getElementsByClassName("card-text")[n];
+				cardTexta.classList.toggle('collapse');	
+				cardImage = document.getElementsByClassName("card-img-top")[n];
+				if (detector%2 == 0){
+					cardImage.style.height = "100%"
+					cardImage.style.width = "100%"
+				}
+				else {
+					cardImage.style.height = "20%"
+					cardImage.style.width = "20%"
+				}
 			}
 		}
 	}
+
+//7) si un utilisateur clique sur le bouton gris ==>, la dernière card (en bas à droite) 
+	//va passer en premier (en haut à gauche). 
+	//On va pouvoir faire tourner les cards !
+	let grayArrow = document.getElementsByClassName('my-2')[1];
+	let card= document.getElementsByClassName("row")[1];
+	var grayArrowButton = function() {
+		card.insertBefore(card.lastChild, card.childNodes[0])
+		console.log("io ary")
+	}
+	grayArrow.addEventListener("click", grayArrowButton)
+
+//8) reciproque du 7)
+	let blueArrow = document.getElementsByClassName('my-2')[0];
+	var blueArrowButton = function() {
+		blueArrow.href = "#"
+		card.insertBefore(card.firstChild, card.lastChild.nextSibling)
+		console.log("io le ray")
+	}
+	blueArrow.addEventListener("click", blueArrowButton)
+
+//9)
+	logo = document.querySelector("strong")
+	content = logo.textContent
+	
+	cardText2.addEventListener("onselect", function(){ console.log("Mety") })
